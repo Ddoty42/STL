823aee0eac0131260dee940d9c0178305b86ea68
Dotes <dote@dote.com>
2020-12-05 10:37:51 -0500

    Added exceptions

--- STL/main.cpp
+++ STL/main.cpp
@@ -1,26 +1,46 @@
 #include <iostream>
 #include <algorithm>
+#include <cctype>
+#include <string>
 
 using namespace std;
 
 char character(char, int);
 
 int main() {
+	char start = '?';
+	int offset = 1;
+	char endLetter = 0;
 
-	char a = 'a';
-
-	cout << char(a + 1);
+	try {
+		endLetter = character(start, offset);
+	}
+	catch (string invalidCharacterException) {
+		cout << invalidCharacterException;
+	}
+	catch (const char invalidRangeException) {
+		cout << invalidRangeException;
+	}
+	
+	cout << endLetter;
 
 }
 
 char character(char start, int offset) {
 
+	char newChar;
+
+	newChar = char(start + offset);
+
+	if (!isalpha(start)) {
+		string invalidCharacterException = "ERROR: Enter a letter!\n";
+		throw invalidCharacterException;
+	}
 	
-	cout << "Enter a letter: ";
-	cin >> start;
-
-	cout << "Enter a number greater than 0: ";
-	cin >> offset;
-
-
+	if (!isalpha(newChar)) {
+		string invalidRangeException = "ERROR: Out of Range!\n";
+		throw invalidRangeException;
+	}
+	
+	return newChar;
 }
