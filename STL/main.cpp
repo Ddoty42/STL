4c96409f06ba314b39a5e4a2c86008c250c3d26b
Dotes <dote@dote.com>
2020-12-05 09:41:00 -0500

    Add project files.

---
+++ STL/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+char character(char, int);
+
+int main() {
+
+	char a = 'a';
+
+	cout << char(a + 1);
+
+}
+
+char character(char start, int offset) {
+
+}
